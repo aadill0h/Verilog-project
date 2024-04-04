dd851f051d2e959111f6815f54cc5027a196ee70
doctorbyt <162490263+doctorbyt@users.noreply.github.com>
2024-04-05 00:58:12 +0530

    Update FCIMS_v2.v

--- FCIMS_v2.v
+++ FCIMS_v2.v
@@ -137,7 +137,7 @@ endmodule
 module FCIMS(reset,ctrl,uprice0,uprice1,uprice2,uprice3,ncel0,ncel1,ncel2,ncel3,ct0,ct1,ct2,ct3,fprice0,fprice1,fprice2,fprice3,fprice4,fprice5,fprice6,fprice7,new_ct0,new_ct1,new_ct2,new_ct3,tprice_init0,tprice_init1,tprice_init2,tprice_init3,tprice_init4,tprice_init5,tprice_init6,tprice_init7,tprice_final0,tprice_final1,tprice_final2,tprice_final3,tprice_final4,tprice_final5,tprice_final6,tprice_final7);
 	input uprice0,uprice1,uprice2,uprice3,uprice3,ncel0,ncel1,ncel2,ncel3,ct0,ct1,ct2,ct3,ctrl,reset,tprice_init0,tprice_init1,tprice_init2,tprice_init3,tprice_init4,tprice_init5,tprice_init6,tprice_init7;
 	output fprice0,fprice1,fprice2,fprice3,fprice4,fprice5,fprice6,fprice7,new_ct0,new_ct1,new_ct2,new_ct3,tprice_final0,tprice_final1,tprice_final2,tprice_final3,tprice_final4,tprice_final5,tprice_final6,tprice_final7;
-	wire w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14;
+	wire w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15,w16,w17,w18,w19,w20,w21,w22;
 	
 	not_gate not1(reset,w1);
 	//if reset ==1 or TRUE then system will be reset
@@ -158,11 +158,21 @@ module FCIMS(reset,ctrl,uprice0,uprice1,uprice2,uprice3,ncel0,ncel1,ncel2,ncel3,
 	and_gate and12(ct3,w1,w13);
 	and_gate and13(ctrl,w1,w14);
 	
-	//and_gate and14(
+	and_gate and14(tprice_init0,w1,w15);
+	and_gate and15(tprice_init1,w1,w16);
+	and_gate and16(tprice_init2,w1,w17);
+	and_gate and17(tprice_init3,w1,w18);
+	and_gate and18(tprice_init4,w1,w19);
+	and_gate and19(tprice_init5,w1,w20);
+	and_gate and20(tprice_init6,w1,w21);
+	and_gate amd21(tprice_init7,w1,w22);
 	
-	adder_8bit add_prod(tprice_init0,tprice_init1,tprice_init2,tprice_init3,tprice_init4,tprice_init5,tprice_init6,tprice_init7,fprice0,fprice1,fprice2,fprice3,fprice4,fprice5,fprice6,fprice7,tprice_final0,tprice_final1,tprice_final2,tprice_final3,tprice_final4,tprice_final5,tprice_final6,tprice_final7,ctrl);
-	adder_subtracter ad_sub1(w10,w11,w12,w13,w6,w7,w8,w9,w14,new_ct0,new_ct1,new_ct2,new_ct3);
+	
+	//and_gate  all the input of prev price
+	//change ad_sub1 so that it becomes total price-price and not price-totalprice
 	multiplier_4_bit multi1(w2,w3,w4,w5,w6,w7,w8,w9,fprice0,fprice1,fprice2,fprice3,fprice4,fprice5,fprice6,fprice7);
+	adder_8bit add_prod(fprice0,fprice1,fprice2,fprice3,fprice4,fprice5,fprice6,fprice7,w15,w16,w17,w18,w19,w20,w21,w22,tprice_final0,tprice_final1,tprice_final2,tprice_final3,tprice_final4,tprice_final5,tprice_final6,tprice_final7,ctrl);
+	adder_subtracter ad_sub1(w6,w7,w8,w9,w10,w11,w12,w13,w14,new_ct0,new_ct1,new_ct2,new_ct3);
 	
 endmodule
 
